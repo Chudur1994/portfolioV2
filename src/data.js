39575f48a4f8a5494cf45fab4201a8dcba8b3e82
Anxious Man <chudur1994@gmail.com>
2018-10-23 16:40:02 -0400

    renamed dist to doc for github pages

--- src/data.js
+++ src/data.js
@@ -2,37 +2,37 @@ const projects = {
   completed: [
     {
       name: "Project 1",
-      image: "/myImage.jpeg",
+      image: "myImage.jpeg",
       description: "Description of the project",
       url: "www.google.com"
     },
     {
       name: "Project 2",
-      image: "/myImage.jpeg",
+      image: "myImage.jpeg",
       description: "Description of the project",
       url: "www.google.com"
     },
     {
       name: "Project 3",
-      image: "/myImage.jpeg",
+      image: "myImage.jpeg",
       description: "Description of the project",
       url: "www.google.com"
     },
     {
       name: "Project 4",
-      image: "/myImage.jpeg",
+      image: "myImage.jpeg",
       description: "Description of the project",
       url: "www.google.com"
     },
     {
       name: "Project 5",
-      image: "/myImage.jpeg",
+      image: "myImage.jpeg",
       description: "Description of the project",
       url: "www.google.com"
     },
     {
       name: "Project 6",
-      image: "/myImage.jpeg",
+      image: "myImage.jpeg",
       description: "Description of the project",
       url: "www.google.com"
     }
@@ -40,25 +40,25 @@ const projects = {
   incomplete: [
     {
       name: "Project 1",
-      image: "/myImage.jpeg",
+      image: "myImage.jpeg",
       description: "Description of the project",
       url: "www.google.com"
     },
     {
       name: "Project 2",
-      image: "/myImage.jpeg",
+      image: "myImage.jpeg",
       description: "Description of the project",
       url: "www.google.com"
     },
     {
       name: "Project 3",
-      image: "/myImage.jpeg",
+      image: "myImage.jpeg",
       description: "Description of the project",
       url: "www.google.com"
     },
     {
       name: "Project 4",
-      image: "/myImage.jpeg",
+      image: "myImage.jpeg",
       description: "Description of the project",
       url: "www.google.com"
     }
--- tmp/data.js
+++ tmp/data.js
@@ -3,53 +3,53 @@
 var projects = {
   completed: [{
     name: "Project 1",
-    image: "/myImage.jpeg",
+    image: "myImage.jpeg",
     description: "Description of the project",
     url: "www.google.com"
   }, {
     name: "Project 2",
-    image: "/myImage.jpeg",
+    image: "myImage.jpeg",
     description: "Description of the project",
     url: "www.google.com"
   }, {
     name: "Project 3",
-    image: "/myImage.jpeg",
+    image: "myImage.jpeg",
     description: "Description of the project",
     url: "www.google.com"
   }, {
     name: "Project 4",
-    image: "/myImage.jpeg",
+    image: "myImage.jpeg",
     description: "Description of the project",
     url: "www.google.com"
   }, {
     name: "Project 5",
-    image: "/myImage.jpeg",
+    image: "myImage.jpeg",
     description: "Description of the project",
     url: "www.google.com"
   }, {
     name: "Project 6",
-    image: "/myImage.jpeg",
+    image: "myImage.jpeg",
     description: "Description of the project",
     url: "www.google.com"
   }],
   incomplete: [{
     name: "Project 1",
-    image: "/myImage.jpeg",
+    image: "myImage.jpeg",
     description: "Description of the project",
     url: "www.google.com"
   }, {
     name: "Project 2",
-    image: "/myImage.jpeg",
+    image: "myImage.jpeg",
     description: "Description of the project",
     url: "www.google.com"
   }, {
     name: "Project 3",
-    image: "/myImage.jpeg",
+    image: "myImage.jpeg",
     description: "Description of the project",
     url: "www.google.com"
   }, {
     name: "Project 4",
-    image: "/myImage.jpeg",
+    image: "myImage.jpeg",
     description: "Description of the project",
     url: "www.google.com"
   }]
