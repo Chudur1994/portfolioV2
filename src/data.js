50535d235bb6bcf9c0ae42c886cf6e5097e00934
uxb6317 <uxb6317@rit.edu>
2018-11-01 16:52:28 -0400

    added todo project

--- src/data.js
+++ src/data.js
@@ -6,44 +6,14 @@ const projects = {
       description:
         "An e-commerce website where you can buy any of the second generation of pokemons.",
       url: "https://chudur1994.github.io/pokestoreV2/"
-    },
-    {
-      name: "Project 2",
-      image: "images/myImage.jpeg",
-      description: "Description of the project",
-      url: "www.google.com"
-    },
-    {
-      name: "Project 3",
-      image: "images/myImage.jpeg",
-      description: "Description of the project",
-      url: "www.google.com"
     }
   ],
   incomplete: [
     {
-      name: "Project 1",
-      image: "images/myImage.jpeg",
-      description: "Description of the project",
-      url: "www.google.com"
-    },
-    {
-      name: "Project 2",
-      image: "images/myImage.jpeg",
-      description: "Description of the project",
-      url: "www.google.com"
-    },
-    {
-      name: "Project 3",
-      image: "images/myImage.jpeg",
-      description: "Description of the project",
-      url: "www.google.com"
-    },
-    {
-      name: "Project 5",
-      image: "images/myImage.jpeg",
-      description: "Description of the project",
-      url: "www.google.com"
+      name: "To-Do",
+      image: "images/todo.PNG",
+      description: "Simple to do app.",
+      url: "https://chudur1994.github.io/ToDo/"
     }
   ]
 };
--- tmp/data.js
+++ tmp/data.js
@@ -6,36 +6,11 @@ var projects = {
     image: "images/pokeStore.PNG",
     description: "An e-commerce website where you can buy any of the second generation of pokemons.",
     url: "https://chudur1994.github.io/pokestoreV2/"
-  }, {
-    name: "Project 2",
-    image: "images/myImage.jpeg",
-    description: "Description of the project",
-    url: "www.google.com"
-  }, {
-    name: "Project 3",
-    image: "images/myImage.jpeg",
-    description: "Description of the project",
-    url: "www.google.com"
   }],
   incomplete: [{
-    name: "Project 1",
-    image: "images/myImage.jpeg",
-    description: "Description of the project",
-    url: "www.google.com"
-  }, {
-    name: "Project 2",
-    image: "images/myImage.jpeg",
-    description: "Description of the project",
-    url: "www.google.com"
-  }, {
-    name: "Project 3",
-    image: "images/myImage.jpeg",
-    description: "Description of the project",
-    url: "www.google.com"
-  }, {
-    name: "Project 5",
-    image: "images/myImage.jpeg",
-    description: "Description of the project",
-    url: "www.google.com"
+    name: "To-Do",
+    image: "images/todo.PNG",
+    description: "Simple to do app.",
+    url: "https://chudur1994.github.io/ToDo/"
   }]
 };
